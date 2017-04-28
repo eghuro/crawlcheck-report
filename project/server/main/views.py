0616e58e40481af72ba7a7f45ea6e6fe5b4f94f9
Alexandr Mansurov <alex@eghuro.cz>
2017-04-28 16:59:02 +0200

    Transaction view

--- project/server/main/views.py
+++ project/server/main/views.py
@@ -28,3 +28,7 @@ def home():
 @main_blueprint.route("/about/")
 def about():
     return render_template("main/about.html")
+
+@main_blueprint.route('/transaction')
+def transactions():
+    return render_template('main/transaction.html')
