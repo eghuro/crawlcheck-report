5374faf1d33a3663afed038ce8c8c55c4adcb03a
Alexandr Mansurov <alex@eghuro.cz>
2017-04-30 21:36:18 +0200

    Wiping DB

--- project/server/main/rest.py
+++ project/server/main/rest.py
@@ -6,6 +6,8 @@
 #################
 
 from flask import Blueprint, request
+from project.server import db
+from project.server.models import Transaction, Finding, Link, Defect, DefectType
 
 
 ################
@@ -23,7 +25,15 @@ rest_blueprint = Blueprint('rest', __name__,)
 @rest_blueprint.route('/data', methods = ['DELETE'])
 def wipe():
     if request.method == 'DELETE':
-        return "Wipe data"
+        cnt = 0
+        tables = [Transaction, Finding, Link, Defect, DefectType]
+        for table in tables:
+            records = table.query.all()
+            for record in records:
+                db.session.delete(record)
+                cnt = cnt + 1
+        db.session.commit()
+        return "Wipe data: " + str(cnt)
 
 @rest_blueprint.route('/data', methods = ['POST'])
 def update():
